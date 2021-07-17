4ab9f9f8905a01ffb50b0c3feb6da905f4aeaa2e
GopalSaraf1 <87574494+GopalSaraf1@users.noreply.github.com>
2021-07-17 21:45:03 +0530

    Update status_message_fn.py

--- tobrot/plugins/status_message_fn.py
+++ tobrot/plugins/status_message_fn.py
@@ -32,15 +32,16 @@ async def upload_as_video(client, message):
     
     
 async def status_message_f(client, message):
-    aria_i_p = await aria_start()
+'''    aria_i_p = await aria_start()
     # Show All Downloads
     downloads = aria_i_p.get_downloads()
     #
     DOWNLOAD_ICON = "📥"
     UPLOAD_ICON = "📤"
     #
+'''
     msg = ""
-    for download in downloads:
+'''    for download in downloads:
         downloading_dir_name = "NA"
         try:
             downloading_dir_name = str(download.name)
@@ -77,7 +78,7 @@ async def status_message_f(client, message):
         if msg == "":
             msg = "🤷‍♂️ No Active, Queued or Paused TORRENTs"
 
-   
+   '''
     
     hr, mi, se = up_time(time.time() - BOT_START_TIME)
     total, used, free = shutil.disk_usage(".")
@@ -253,7 +254,6 @@ async def upload_log_file(client, message):
         await message.reply_document("TorrentLeech.txt")
         
 async def help_message_f(client, message):
-#    aria_i_p = await aria_start()
     
     msg = ("""📖 Help
     
