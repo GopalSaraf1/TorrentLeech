cbd89e45df1383c23d40a8bbda7eb36cf176af2e
GopalSaraf1 <87574494+GopalSaraf1@users.noreply.github.com>
2021-07-17 20:27:33 +0530

    Update status_message_fn.py

--- tobrot/plugins/status_message_fn.py
+++ tobrot/plugins/status_message_fn.py
@@ -32,7 +32,7 @@ async def upload_as_video(client, message):
     
     
 async def status_message_f(client, message):
-    aria_i_p = await aria_start()
+#    aria_i_p = await aria_start()
     # Show All Downloads
     downloads = aria_i_p.get_downloads()
     #
